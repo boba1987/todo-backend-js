db8fbccd499af10d860ceb7c82d5466730e0ef6f
Slobodan Djordjevic <slobodan.djordjevic@getvoip.com>
2021-08-27 16:37:08 +0200

    Add todo model

---
+++ src/models/todo.model.js
@@ -0,0 +1,34 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const todo = sequelizeClient.define('todo', {
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  todo.associate = function (models) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+
+    todo.belongsTo(models.users, {
+      foreignKey: {
+        name: 'id'
+      }
+    });
+  };
+
+  return todo;
+};
